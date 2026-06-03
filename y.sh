9602c90f07d38e51dee1288a5b511468da73d9b5
sahilt324gg-web <sahilt.324.gg@gmail.com>
2026-06-03 17:18:04 +0530

    Create y.sh

---
+++ y.sh
@@ -0,0 +1,7 @@
+sudo apt update
+sudo apt install python3 python3-pip -y
+
+python -m pip install python-telegram-bot selenium undetected-chromedriver
+
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo apt install ./google-chrome-stable_current_amd64.deb -y
